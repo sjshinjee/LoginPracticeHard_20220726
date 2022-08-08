b7d261cea4d6ce1600d3aa3afe78cefa01bdcea6
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-08 12:32:03 +0900

    틀린 값으로 계산할 때는 그 값만 계산이 된다 그래서 이 코드는 잘못된 코드다 //이해 안됨 ㅠㅠ

--- app/src/main/java/com/homee/loginpracticehard_20220726/MainActivity.kt
+++ app/src/main/java/com/homee/loginpracticehard_20220726/MainActivity.kt
@@ -13,13 +13,26 @@ class MainActivity : AppCompatActivity() {
         loginBtn.setOnClickListener {
             val inputId = idEdt.text.toString()
             val inputPw = passwordEdt.text.toString()
-//논리 연산자(&&)를 활용해서 id와 pw를 한번에 분기처리
-            if(inputId == "admin" && inputPw == "asdf"){
-                Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
-            }
-            else{
+
+           //이렇게 짤 경우 아무것도 id와 pw에 안넣고 로그인 버튼 누르면 "관리자님 환영합니다"뜬다 그래서 틀린 값
+            //으로 계산할 때는 그 값만 계산이 된다
+            if(inputId != "admin"){
                 Toast.makeText(this, "로그인에 실패하셨습니다", Toast.LENGTH_SHORT).show()
             }
+            else if(inputPw != "asdf"){
+                Toast.makeText(this, "로그인에 실패하셨습니다", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
+            }
+
+////논리 연산자(&&)를 활용해서 id와 pw를 한번에 분기처리
+//            if(inputId == "admin" && inputPw == "asdf"){
+//                Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
+//            }
+//            else{
+//                Toast.makeText(this, "로그인에 실패하셨습니다", Toast.LENGTH_SHORT).show()
+//            }
 
 
         }
